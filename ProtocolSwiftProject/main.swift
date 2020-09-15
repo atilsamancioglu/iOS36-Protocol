b1fdd41c82882ae60a6b748094550386537ac75f
Atil Samancioglu <samancioglu.atil@gmail.com>
2020-09-15 13:57:29 +0300

    first commit

--- ProtocolSwiftProject/main.swift
+++ ProtocolSwiftProject/main.swift
@@ -7,5 +7,48 @@
 
 import Foundation
 
-print("Hello, World!")
+protocol Running {
+    func myRun()
+}
+
+class Animal {
+    func test() {
+        print("test")
+    }
+}
+
+class Dog : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let barley = Dog()
+barley.myRun()
+
+class Cat : Animal, Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let cat = Cat()
+cat.test()
+cat.myRun()
+
+class Turtle : Animal {
+    
+}
+
+let leonardo = Turtle()
+
+
+struct Bird : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let tweety = Bird()
+tweety.myRun()
 
